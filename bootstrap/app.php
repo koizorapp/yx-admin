10b44b80a8737b7f1c30043194c0319b29e1786f
koizora <koizora_pp@163.com>
2017-11-13 16:50:07 +0800

    日志记录按天记录

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,14 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+
+$app->configureMonologUsing(function(Monolog\Logger $monolog) {
+    $fileName = storage_path('logs/laravel-' . php_sapi_name() . '.log');
+    $handler  = new Monolog\Handler\RotatingFileHandler($fileName);
+    $monolog->pushHandler($handler);
+});
+
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
