56914d80eb72a9d0a4e876e30cf3ca509070aeb4
liufei <1196778674@qq.com>
2017-11-17 16:16:40 +0800

    build

--- resources/归档/src/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-window.jQuery = window.$ = require('jquery');
-$(function() {
-    // 进入自执行的一些方法
-    Public.autoLeftNav();
-    $(window).resize(function() {
-        Public.autoLeftNav();
-    });
-});
